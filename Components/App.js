adb6156f0266f9694349159290129a8cf9dc1b3e
Habert <habertotieno4@gmail.com>
2025-04-24 08:36:36 +0300

    updated country display

---
+++ Components/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import CountryDisplay from './CountryDisplay'
+
+function App() {
+  return (
+    <div><CountryDisplay /></div>
+  )
+}
+
+export default App
--- Components/CountryDisplay.js
+++ Components/CountryDisplay.js
@@ -1,21 +1,30 @@
+import React from 'react'
+import {useState,useEffect} from 'react'
+const CountryDisplay = () => {
+  const[countries,setCountries]=useState([])
+useEffect(()=>{
+fetch("https://restcountries.com/v3.1/all")
+.then(res =>res.json())
+.then(data =>setCountries(data))
+},[])
+let newCountries=countries.map(country=>{
+  return(
+    <div className='country-display' key={country.id}>
+ <h1>{country.name.common}</h1>
+ <p><strong>Official Name:</strong>{country.name.official}</p>
+ <p><strong>Flag:</strong> {country.flag}</p>
+ <p><strong>Region:</strong>{country.region}</p>
+ <p><strong>Capital:</strong>{country.capital}</p>
+ <p><strong>Area: </strong> {country.area}</p>
+<p><strong>Population:</strong> {country.population}</p>
 
-import React from "react";
-
-export default function CountryDisplay({ country }) {
-  if (!country) return null;
-
-  return (
-    <div className="mt-4 border p-4 rounded shadow">
-      <h2 className="text-xl font-semibold mb-2">{country.name.common}</h2>
-      <img
-        src={country.flags.png}
-        alt={`Flag of ${country.name.common}`}
-        className="w-32 h-auto mb-2"
-      />
-      <p><strong>Capital:</strong> {country.capital ? country.capital[0] : "N/A"}</p>
-      <p><strong>Region:</strong> {country.region || "N/A"}</p>
-      <p><strong>Subregion:</strong> {country.subregion || "N/A"}</p>
-      <p><strong>Borders:</strong> {country.borders ? country.borders.join(", ") : "None"}</p>
     </div>
-  );
+  )
+})
+return(
+    <div>{newCountries}</div>
+)
+
 }
+export default CountryDisplay
+
